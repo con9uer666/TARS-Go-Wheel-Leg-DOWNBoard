63999a90531406d7db631cfcd23baf447fd2d3de
张玉锦 <terry20070719@163.com>
2026-05-12 20:02:05 +0800

    重调了零点

--- others/Motion_Control/App/src/motor.c
+++ others/Motion_Control/App/src/motor.c
@@ -304,8 +304,8 @@ void task_VMC_Init()
 //PID赋值与初始化结构体
 void task_PID_Init()
 {
-    PID_INIT(&L_Leg_L0_PID, 5000, 0, 50000, 200, 0, 0, 0, 0);
-    PID_INIT(&R_Leg_L0_PID, 5000, 0, 50000, 200, 0, 0, 0, 0);
+    PID_INIT(&L_Leg_L0_PID, 2500, 0, 30000, 200, 0, 0, 0, 0);
+    PID_INIT(&R_Leg_L0_PID, 2500, 0, 30000, 200, 0, 0, 0, 0);
     PID_INIT(&Leg_Phi0_PID, 300, 0, 5, 150, 150, 0, 50000, 0);
     PID_INIT(&Roll_Comp_PID, 10, 0.002, 100, 150, 80, 0, 10000, 0);
 
@@ -320,7 +320,7 @@ void task_PID_Init()
     PID_INIT(&R_Leg_L0_SPD_PID, 200, 0.03, 50, 80, 80, 0, 2000, 0);
 
     //小陀螺pid
-    PID_INIT(&spinning_pid, 0, 0.003f, 0, 6.0f, 6.0f, 0.008f, 20.0f, 0);
+    PID_INIT(&spinning_pid, 0, 0.0f, 0, 6.0f, 6.0f, 0.008f, 20.0f, 0);
 
     //云台pid
     PID_INIT(&gimbal_pitch_pid, 10, 0.002, 100, 150, 80, 0, 10000, 0);
@@ -865,8 +865,8 @@ void Standing()
 
     //常态下VMC解算，加入PID前馈
     float centrifugal_comp = centrifugal_comp_gain * d_yaw * d_yaw;
-    VMC_Set_F0_T(&VMC_L, L_Leg_L0_PID.output + (mg / arm_cos_f32(VMC_L.b_phi0)) + Roll_Comp_PID.output, Leg_L_T + Leg_Phi0_PID.output);
-    VMC_Set_F0_T(&VMC_R, R_Leg_L0_PID.output + (mg / arm_cos_f32(VMC_R.b_phi0)) - Roll_Comp_PID.output, -Leg_R_T + Leg_Phi0_PID.output);
+    VMC_Set_F0_T(&VMC_L, L_Leg_L0_PID.output + (mg / arm_cos_f32(VMC_L.b_phi0)) + Roll_Comp_PID.output + centrifugal_comp, Leg_L_T + Leg_Phi0_PID.output);
+    VMC_Set_F0_T(&VMC_R, R_Leg_L0_PID.output + (mg / arm_cos_f32(VMC_R.b_phi0)) - Roll_Comp_PID.output + centrifugal_comp, -Leg_R_T + Leg_Phi0_PID.output);
 
     off_ground_detect();
 
@@ -1161,10 +1161,10 @@ void Gravity_Compensation_Test_Function(void)
 
 /*====================================== 坐地模式 =========================================== */
 
-#define SIT_TARGET_L0_L      0.172f
-#define SIT_TARGET_L0_R      0.173f
-#define SIT_TARGET_PHI0_L    0.749f
-#define SIT_TARGET_PHI0_R    2.319f
+#define SIT_TARGET_L0_L      0.1532f
+#define SIT_TARGET_L0_R      0.1555f
+#define SIT_TARGET_PHI0_L    0.7353f
+#define SIT_TARGET_PHI0_R    2.3164f
 #define SIT_RAMP_TIME        0.8f
 #define SIT_WHEEL_TORQUE     0.0f
 #define SIT_GRAVITY_RATIO    0.3f
